aea970b0a4a683f510aa5d22b92bb24b2da137f8
Bill Lotter <blotter23@gmail.com>
2016-04-07 15:12:44 -0400

    added bias layer

--- keras/layers/core.py
+++ keras/layers/core.py
@@ -1164,6 +1164,71 @@ class Dense(Layer):
         return dict(list(base_config.items()) + list(config.items()))
 
 
+class Bias(Layer):
+    input_ndim = 2
+
+    def __init__(self, output_dim, init='glorot_uniform', activation='linear', weights=None,
+                b_regularizer=None, activity_regularizer=None,
+                b_constraint=None, input_dim=None, **kwargs):
+        self.init = initializations.get(init)
+        self.activation = activations.get(activation)
+        self.output_dim = output_dim
+
+        self.b_regularizer = regularizers.get(b_regularizer)
+        self.activity_regularizer = regularizers.get(activity_regularizer)
+
+        self.b_constraint = constraints.get(b_constraint)
+        self.constraints = [self.b_constraint]
+
+        self.initial_weights = weights
+
+        self.input_dim = input_dim
+        if self.input_dim:
+            kwargs['input_shape'] = (self.input_dim,)
+        super(Dense, self).__init__(**kwargs)
+
+    def build(self):
+        input_dim = self.input_shape[1]
+
+        self.b = K.zeros((self.output_dim,),
+                         name='{}_b'.format(self.name))
+
+        self.trainable_weights = [self.b]
+
+        if self.b_regularizer:
+            self.b_regularizer.set_param(self.b)
+            self.regularizers.append(self.b_regularizer)
+
+        if self.activity_regularizer:
+            self.activity_regularizer.set_layer(self)
+            self.regularizers.append(self.activity_regularizer)
+
+        if self.initial_weights is not None:
+            self.set_weights(self.initial_weights)
+            del self.initial_weights
+
+    @property
+    def output_shape(self):
+        return (self.input_shape[0], self.output_dim)
+
+    def get_output(self, train=False):
+        X = self.get_input(train)
+        output = self.activation(X + self.b)
+        return output
+
+    def get_config(self):
+        config = {'name': self.__class__.__name__,
+                  'output_dim': self.output_dim,
+                  'init': self.init.__name__,
+                  'activation': self.activation.__name__,
+                  'b_regularizer': self.b_regularizer.get_config() if self.b_regularizer else None,
+                  'activity_regularizer': self.activity_regularizer.get_config() if self.activity_regularizer else None,
+                  'b_constraint': self.b_constraint.get_config() if self.b_constraint else None,
+                  'input_dim': self.input_dim}
+        base_config = super(Dense, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+
 class TimeDistributedDense(MaskedLayer):
     '''Apply a same Dense layer for each dimension[1] (time_dimension) input.
     Especially useful after a recurrent network with 'return_sequence=True'.
