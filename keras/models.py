a106fe5167601ea634a057111a11b18c21933e51
Bill Lotter <blotter23@gmail.com>
2016-06-27 19:33:36 -0400

    debugging predict_generator

--- keras/models.py
+++ keras/models.py
@@ -1483,7 +1483,7 @@ class Graph(Model, containers.Graph):
             do_samples = len(data[next(iter(data.keys()))])
             outs = self.predict(data, batch_size=do_samples,
                                  verbose=verbose)
-            all_outs = all_outs.extend(outs[pred_layer].tolist())
+            all_outs += outs[pred_layer].tolist()
 
             done_samples += do_samples
             weights.append(do_samples)
