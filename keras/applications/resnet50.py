5a90ba11cab6884ca6109400b7683eea2aba2265
Bill Lotter <blotter23@gmail.com>
2018-06-15 17:03:33 -0400

    Update batchnorm options in resnet50

--- keras/applications/resnet50.py
+++ keras/applications/resnet50.py
@@ -40,7 +40,7 @@ WEIGHTS_PATH = 'https://github.com/fchollet/deep-learning-models/releases/downlo
 WEIGHTS_PATH_NO_TOP = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.2/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5'
 
 
-def identity_block(input_tensor, kernel_size, filters, stage, block):
+def identity_block(input_tensor, kernel_size, filters, stage, block, use_batch_inf=False, use_batch_train=True):
     """The identity block is the block that has no conv layer at shortcut.
 
     # Arguments
@@ -62,23 +62,23 @@ def identity_block(input_tensor, kernel_size, filters, stage, block):
     bn_name_base = 'bn' + str(stage) + block + '_branch'
 
     x = Conv2D(filters1, (1, 1), name=conv_name_base + '2a')(input_tensor)
-    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
+    x = BatchNormalization(axis=bn_axis, use_batch_inf=use_batch_inf, use_batch_train=use_batch_train, name=bn_name_base + '2a')(x)
     x = Activation('relu')(x)
 
     x = Conv2D(filters2, kernel_size,
                padding='same', name=conv_name_base + '2b')(x)
-    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
+    x = BatchNormalization(axis=bn_axis, use_batch_inf=use_batch_inf, use_batch_train=use_batch_train, name=bn_name_base + '2b')(x)
     x = Activation('relu')(x)
 
     x = Conv2D(filters3, (1, 1), name=conv_name_base + '2c')(x)
-    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
+    x = BatchNormalization(axis=bn_axis, use_batch_inf=use_batch_inf, use_batch_train=use_batch_train, name=bn_name_base + '2c')(x)
 
     x = layers.add([x, input_tensor])
     x = Activation('relu')(x)
     return x
 
 
-def conv_block(input_tensor, kernel_size, filters, stage, block, strides=(2, 2)):
+def conv_block(input_tensor, kernel_size, filters, stage, block, strides=(2, 2), use_batch_inf=False, use_batch_train=True):
     """A block that has a conv layer at shortcut.
 
     # Arguments
@@ -106,20 +106,20 @@ def conv_block(input_tensor, kernel_size, filters, stage, block, strides=(2, 2))
 
     x = Conv2D(filters1, (1, 1), strides=strides,
                name=conv_name_base + '2a')(input_tensor)
-    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
+    x = BatchNormalization(axis=bn_axis, use_batch_inf=use_batch_inf, use_batch_train=use_batch_train, name=bn_name_base + '2a')(x)
     x = Activation('relu')(x)
 
     x = Conv2D(filters2, kernel_size, padding='same',
                name=conv_name_base + '2b')(x)
-    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
+    x = BatchNormalization(axis=bn_axis, use_batch_inf=use_batch_inf, use_batch_train=use_batch_train, name=bn_name_base + '2b')(x)
     x = Activation('relu')(x)
 
     x = Conv2D(filters3, (1, 1), name=conv_name_base + '2c')(x)
-    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
+    x = BatchNormalization(axis=bn_axis, use_batch_inf=use_batch_inf, use_batch_train=use_batch_train, name=bn_name_base + '2c')(x)
 
     shortcut = Conv2D(filters3, (1, 1), strides=strides,
                       name=conv_name_base + '1')(input_tensor)
-    shortcut = BatchNormalization(axis=bn_axis, name=bn_name_base + '1')(shortcut)
+    shortcut = BatchNormalization(axis=bn_axis, use_batch_inf=use_batch_inf, use_batch_train=use_batch_train, name=bn_name_base + '1')(shortcut)
 
     x = layers.add([x, shortcut])
     x = Activation('relu')(x)
@@ -129,7 +129,9 @@ def conv_block(input_tensor, kernel_size, filters, stage, block, strides=(2, 2))
 def ResNet50(include_top=True, weights='imagenet',
              input_tensor=None, input_shape=None,
              pooling=None,
-             classes=1000):
+             classes=1000,
+             use_batch_inf=False,
+             use_batch_train=True):
     """Instantiates the ResNet50 architecture.
 
     Optionally loads weights pre-trained
@@ -212,29 +214,29 @@ def ResNet50(include_top=True, weights='imagenet',
 
     x = ZeroPadding2D(padding=(3, 3), name='conv1_pad')(img_input)
     x = Conv2D(64, (7, 7), strides=(2, 2), padding='valid', name='conv1')(x)
-    x = BatchNormalization(axis=bn_axis, name='bn_conv1')(x)
+    x = BatchNormalization(axis=bn_axis, use_batch_inf=use_batch_inf, use_batch_train=use_batch_train, name='bn_conv1')(x)
     x = Activation('relu')(x)
     x = MaxPooling2D((3, 3), strides=(2, 2))(x)
 
-    x = conv_block(x, 3, [64, 64, 256], stage=2, block='a', strides=(1, 1))
-    x = identity_block(x, 3, [64, 64, 256], stage=2, block='b')
-    x = identity_block(x, 3, [64, 64, 256], stage=2, block='c')
+    x = conv_block(x, 3, [64, 64, 256], stage=2, block='a', strides=(1, 1), use_batch_inf=use_batch_inf, use_batch_train=use_batch_train)
+    x = identity_block(x, 3, [64, 64, 256], stage=2, block='b', use_batch_inf=use_batch_inf, use_batch_train=use_batch_train)
+    x = identity_block(x, 3, [64, 64, 256], stage=2, block='c', use_batch_inf=use_batch_inf, use_batch_train=use_batch_train)
 
-    x = conv_block(x, 3, [128, 128, 512], stage=3, block='a')
-    x = identity_block(x, 3, [128, 128, 512], stage=3, block='b')
-    x = identity_block(x, 3, [128, 128, 512], stage=3, block='c')
-    x = identity_block(x, 3, [128, 128, 512], stage=3, block='d')
+    x = conv_block(x, 3, [128, 128, 512], stage=3, block='a', use_batch_inf=use_batch_inf, use_batch_train=use_batch_train)
+    x = identity_block(x, 3, [128, 128, 512], stage=3, block='b', use_batch_inf=use_batch_inf, use_batch_train=use_batch_train)
+    x = identity_block(x, 3, [128, 128, 512], stage=3, block='c', use_batch_inf=use_batch_inf, use_batch_train=use_batch_train)
+    x = identity_block(x, 3, [128, 128, 512], stage=3, block='d', use_batch_inf=use_batch_inf, use_batch_train=use_batch_train)
 
-    x = conv_block(x, 3, [256, 256, 1024], stage=4, block='a')
-    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='b')
-    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='c')
-    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='d')
-    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='e')
-    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='f')
+    x = conv_block(x, 3, [256, 256, 1024], stage=4, block='a', use_batch_inf=use_batch_inf, use_batch_train=use_batch_train)
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='b', use_batch_inf=use_batch_inf, use_batch_train=use_batch_train)
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='c', use_batch_inf=use_batch_inf, use_batch_train=use_batch_train)
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='d', use_batch_inf=use_batch_inf, use_batch_train=use_batch_train)
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='e', use_batch_inf=use_batch_inf, use_batch_train=use_batch_train)
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='f', use_batch_inf=use_batch_inf, use_batch_train=use_batch_train)
 
-    x = conv_block(x, 3, [512, 512, 2048], stage=5, block='a')
-    x = identity_block(x, 3, [512, 512, 2048], stage=5, block='b')
-    x = identity_block(x, 3, [512, 512, 2048], stage=5, block='c')
+    x = conv_block(x, 3, [512, 512, 2048], stage=5, block='a', use_batch_inf=use_batch_inf, use_batch_train=use_batch_train)
+    x = identity_block(x, 3, [512, 512, 2048], stage=5, block='b', use_batch_inf=use_batch_inf, use_batch_train=use_batch_train)
+    x = identity_block(x, 3, [512, 512, 2048], stage=5, block='c', use_batch_inf=use_batch_inf, use_batch_train=use_batch_train)
 
     x = AveragePooling2D((7, 7), name='avg_pool')(x)
     #x = GlobalAveragePooling2D(name='avg_pool')(x)
